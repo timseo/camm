719a4a2dfea896420f899ff35966f11c2755e0e5
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2022-11-11 22:16:53 +0600

    application page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,23 @@ $(document).ready(function(){
         e.preventDefault();
         $(this).toggleClass('active').siblings().slideToggle();
     })
+    $('.content-wrap').css({'height':$('.content-wrap').height()})
+    $(window).scroll(function(){
+        if($(this).scrollTop() > $('.header').height() ){
+            $('.product-section .product-content').addClass('sticky');
+        }else{
+            $('.product-section .product-content').removeClass('sticky');
+        }
+    })
+    $('.product img').click(function(e){
+        e.preventDefault();
+        var ht = $(this).siblings().children().clone();
+        console.log(ht)
+        $('.product-content .product-detais').empty()
+        $('.product-content .product-detais').append(ht);
+        $('.product').removeClass('active')
+        $(this).parents().addClass('active')
+    })
 })
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
@@ -20,4 +37,24 @@ var swiper = new Swiper(".mySwiper", {
         spaceBetween: 65,
         },
     },
+});
+var swiper = new Swiper(".product-slider1", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+});
+var swiper = new Swiper(".product-slider2", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+});
+var swiper = new Swiper(".product-slider3", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+});
+var swiper = new Swiper(".product-slider4", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+});
+var swiper = new Swiper(".product-slider5", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
 });
