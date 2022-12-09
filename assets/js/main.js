cfc8de692d46642498c2d41abd116820f649cc10
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2022-12-09 20:43:21 +0600

    update ---9-12-2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
 (function($) {
     //Hide Loading Box (Preloader)
-	// function handlePreloader() {
-	// 	if($('.loader-wrap').length){
-	// 		$('.loader-wrap').delay(2200).fadeOut(500);
-	// 	}
-	// }
-    // $(window).on('load', function() {
-	// 	handlePreloader();
-	// });
+	function handlePreloader() {
+		if($('.loader-wrap').length){
+			$('.loader-wrap').delay(2200).fadeOut(500);
+		}
+	}
+    $(window).on('load', function() {
+		handlePreloader();
+	});
+
+    
+    var a = $('#vid').clone();
+    $('#vid').empty()
+    
+    setTimeout(function() {
+        $('#vid').delay(2000).append(a);
+    }, 2200);
 
 
 // Hide header on scroll down
@@ -122,23 +130,59 @@ function hasScrolled() {
     var swiper = new Swiper(".product-slider1", {
         slidesPerView: "auto",
         spaceBetween: 20,
+        navigation: {
+            nextEl: ".product-slider1 .swiper-button-next",
+            prevEl: ".product-slider1 .swiper-button-prev",
+        },
     });
     var swiper = new Swiper(".product-slider2", {
         slidesPerView: "auto",
         spaceBetween: 20,
+        navigation: {
+            nextEl: ".product-slider2 .swiper-button-next",
+            prevEl: ".product-slider2 .swiper-button-prev",
+        },
     });
     var swiper = new Swiper(".product-slider3", {
         slidesPerView: "auto",
         spaceBetween: 20,
+        navigation: {
+            nextEl: ".product-slider3 .swiper-button-next",
+            prevEl: ".product-slider3 .swiper-button-prev",
+        },
     });
     var swiper = new Swiper(".product-slider4", {
         slidesPerView: "auto",
         spaceBetween: 20,
+        navigation: {
+            nextEl: ".product-slider4 .swiper-button-next",
+            prevEl: ".product-slider4 .swiper-button-prev",
+        },
     });
     var swiper = new Swiper(".product-slider5", {
         slidesPerView: "auto",
         spaceBetween: 20,
+        navigation: {
+            nextEl: ".product-slider5 .swiper-button-next",
+            prevEl: ".product-slider5 .swiper-button-prev",
+        },
     });
 
 
 })(window.jQuery);
+
+// $(document).ready(function(){
+        //   $(window).scroll(function(){
+        //     var wscroll = $(this).scrollTop();
+        //     if(wscroll>$('.home-banner-video').offset().top + ($(window).height()/3)){
+        //       $('.home-banner-video h3').addClass('show');
+        //     }else{
+        //       $('.home-banner-video h3').removeClass('show');
+        //     }
+        //     if(wscroll>$('.home-banner-video').offset().top + ($(window).height()/3) + $('.home-banner-video h3').height()*1.5){
+        //       $('.home-banner-video p').addClass('show');
+        //     }else{
+        //       $('.home-banner-video p').removeClass('show');
+        //     }
+        //   })
+        // })
